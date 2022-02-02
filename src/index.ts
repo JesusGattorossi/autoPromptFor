73853a183685e6fb33d987c2488ef6f4c5543fec
JesusGattorossi <93558045+JesusGattorossi@users.noreply.github.com>
2022-02-01 23:09:43 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+//calcular tiempo total; promedio de vuelta
+/*
+let vuelta1: number = Number(prompt("ngrese tiempo de vuelta1"));
+let vuelta2: number = Number(prompt("ngrese tiempo de vuelta2"));
+let vuelta3: number = Number(prompt("ngrese tiempo de vuelta3"));
+let vuelta4: number = Number(prompt("ngrese tiempo de vuelta4"));
+//let tiempoTotal:number= vuelta1+vuelta2+vuelta3+vuelta4;
+//console.log( "el tiempo total de la carrera fue", tiempoTotal);
+//let promedio: number= tiempoTotal/4;
+//console.log("el promedio de vuelta es", promedio);
+*/
+/*let sumaVueltas: number = vuelta1 + vuelta2 + vuelta3 + vuelta4;
+let contador: number = 1;
+for (contador === vuelta1; contador <= vuelta4; contador++) {
+  console.log("la suma de las vueltas es " + sumaVueltas);
+ 
+}
+let promedio: number = sumaVueltas / 4;
+console.log("el promedio de vuelta es", promedio);
+*/
